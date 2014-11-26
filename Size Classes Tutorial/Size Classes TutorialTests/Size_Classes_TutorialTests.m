eb7650bde2385c89ad95a3a3a24ca084fc1c8d4b
Hossam Ghareeb <hossam.ghareb@gmail.com>
2014-11-26 19:24:05 +0200

    Creating the project

---
+++ Size Classes Tutorial/Size Classes TutorialTests/Size_Classes_TutorialTests.m
@@ -0,0 +1,40 @@
+//
+//  Size_Classes_TutorialTests.m
+//  Size Classes TutorialTests
+//
+//  Created by Hossam Ghareeb on 11/26/14.
+//  Copyright (c) 2014 Hossam Ghareeb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Size_Classes_TutorialTests : XCTestCase
+
+@end
+
+@implementation Size_Classes_TutorialTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
